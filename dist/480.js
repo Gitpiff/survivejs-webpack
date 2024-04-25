17911692998b27ce365e07eb0502e086f441683e
Victor <viknavarro@gmail.com>
2024-04-25 15:51:39 +0100

    code splitting

---
+++ dist/480.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksurvivejs=self.webpackChunksurvivejs||[]).push([[480],{480:(s,e,u)=>{u.r(e),u.d(e,{default:()=>l});const l="Hello from lazy"}}]);
--- src/component.js
+++ src/component.js
@@ -2,5 +2,13 @@ export default (text = "Hello from component inside src") => {
     const element = document.createElement('div');
     element.className = "rounded bg-red-100 border max-w-md m-4 p-4";
     element.innerHTML = text;
+
+    element.onclick= () => {
+        import("./lazy")
+            .then((lazy) => {
+                element.textContent = lazy.default;
+            })
+            .catch((error) => console.log(error));
+    }
     return element
 };
